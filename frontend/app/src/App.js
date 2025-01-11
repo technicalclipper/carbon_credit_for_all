4ab88aaf86d73edbe4dd47d509c2aa3115e3c3dd
nikhilpickle22 <128140760+nikhilpickle22@users.noreply.github.com>
2025-01-11 12:51:44 +0530

    Sign Up Init

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,9 +1,54 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
+import ReactDOM from 'react-dom'
 import './App.css';
 
+function Signup(){
+  const [inputs, setInputs] = useState({});
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({...values, [name]: value}))
+    {console.log(name)}
+  }
+  return(
+    <div className="register">
+        <h1 >sign up</h1>
+        <form>
+          <label>Name:
+            <input 
+              name='username' 
+              type='text' 
+              value={inputs.username || ""} 
+              onChange={handleChange}
+            ></input>
+          </label>
+          <label>Email:
+            <input 
+              name='Email' 
+              type='text' 
+              value={inputs.Email || ""} 
+              onChange={handleChange}
+            ></input>
+          </label>
+          <label>Password:
+            <input 
+              name='password' 
+              type='password' 
+              value={inputs.password || ""} 
+              onChange={handleChange}
+            ></input>
+            <button type='submit'>Sign Up</button>
+          </label>          
+        </form>
+        <p>{}</p>
+      </div>
+      
+  )
+}
 function App() {
   return (
     <>
+      <Signup / >
     </>
   );
 }
