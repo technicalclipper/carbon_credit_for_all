da9ef8335f4702f437bf2ec761b4296215bbca69
technicalclipper <technicalclipper@gmail.com>
2025-01-11 14:37:25 +0530

    edited app.js

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -4,15 +4,17 @@ import './App.css';
 import Signup from './pages/signup.js'
 import Signin from './pages/signin.js';
 import { Route,Routes } from 'react-router-dom';
-
+import { AuthProvider } from './contexts/authContext';
 
 function App() {
   return (
     <>
+      <AuthProvider>
       <Routes>
         <Route path="/signup" element={<Signup/>}/>
         <Route path="/signin" element={<Signin/>}/>
       </Routes>
+      </AuthProvider>
     </>
   );
 }
