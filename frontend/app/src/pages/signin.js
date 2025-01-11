55c16d84a1dfd74863f82ac647e817b234befbee
nikhilpickle22 <128140760+nikhilpickle22@users.noreply.github.com>
2025-01-11 13:38:21 +0530

    added styles to signup

--- frontend/app/src/pages/signin.js
+++ frontend/app/src/pages/signin.js
@@ -1,46 +1,48 @@
-import React,{useState} from 'react';
-import ReactDOM from 'react-dom'
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "../signin.css";
 
-function Signin(){
-    const [inputs, setInputs] = useState({});
-    const handleChange = (event) => {
-      const name = event.target.name;
-      const value = event.target.value;
-      setInputs(values => ({...values, [name]: value}))
-      {console.log(name)}
+function Signin() {
+  const [inputs, setInputs] = useState({});
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+    {
+      console.log(name);
     }
-    return(
-        <div className="container">
-            <div className="left-panel">
-            <div className="logo">
-                🌴
-            </div>
-            <h1>Platform for Personal Carbon Credits.</h1>
-            </div> 
-            <div className="right-panel">
-            <h2>sign in</h2>
-            <form className="form">
-                <label htmlFor="username">Name:</label>
-                <input 
-                    name='username' 
-                    type='text' 
-                    value={inputs.username || ""} 
-                    onChange={handleChange}
-                />
-                <label htmlFor="password">Password:</label> 
-                <input 
-                    name='password' 
-                    type='password' 
-                    value={inputs.password || ""} 
-                    onChange={handleChange}
-                ></input>
-                <button type='submit' className="submit-button">Sign In</button>
-                         
-            </form>
-            <p>{}</p>
-            </div>
-        </div>    
-    )
+  };
+  return (
+    <div className="container">
+      <div className="left-panel">
+        <div className="logo">🌴</div>
+        <h1>Platform for Personal Carbon Credits.</h1>
+      </div>
+      <div className="right-panel">
+        <form className="form">
+          <h2>sign in</h2>
+          <label htmlFor="username">Name:</label>
+          <input
+            name="username"
+            type="text"
+            value={inputs.username || ""}
+            onChange={handleChange}
+          />
+          <label htmlFor="password">Password:</label>
+          <input
+            name="password"
+            type="password"
+            value={inputs.password || ""}
+            onChange={handleChange}
+          ></input>
+          <button type="submit" className="submit-button">
+            Sign In
+          </button>
+        </form>
+        <p>{}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Signin;
--- frontend/app/src/pages/signup.js
+++ frontend/app/src/pages/signup.js
@@ -1,53 +1,56 @@
-import React,{useState} from 'react';
-import ReactDOM from 'react-dom'
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "../signin.css";
 
-function Signup(){
-    const [inputs, setInputs] = useState({});
-    const handleChange = (event) => {
-      const name = event.target.name;
-      const value = event.target.value;
-      setInputs(values => ({...values, [name]: value}))
-      {console.log(name)}
+function Signup() {
+  const [inputs, setInputs] = useState({});
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+    {
+      console.log(name);
     }
-    return(
-      <div className="container">
-            <div className="left-panel">
-            <div className="logo">
-                🌴
-            </div>
-            <h1>Platform for Personal Carbon Credits.</h1>
-            </div> 
-      <div className="right-panel">
-          <h1 >sign up</h1>
-          <form className="form">
-                <label htmlFor="username">Name:</label>
-                <input 
-                    name='username' 
-                    type='text' 
-                    value={inputs.username || ""} 
-                    onChange={handleChange}
-                />
-            <label htmlFor="Email">Email:
-              <input 
-                name='Email' 
-                type='text' 
-                value={inputs.Email || ""} 
-                onChange={handleChange}
-              ></input>
-            </label>
-            <label htmlFor="password">Password:</label> 
-              <input 
-                name='password' 
-                type='password' 
-                value={inputs.password || ""} 
-                onChange={handleChange}
-              ></input>
-              <button type='submit' className="submit-button">Sign Up</button>       
-          </form>
-        </div>
+  };
+  return (
+    <div className="container">
+      <div className="left-panel">
+        <div className="logo">🌴</div>
+        <h1>Platform for Personal Carbon Credits.</h1>
       </div>
-        
-    )
-  }
+      <div className="right-panel">
+        <form className="form">
+          <h1>sign up</h1>
+          <label htmlFor="username">Name:</label>
+          <input
+            name="username"
+            type="text"
+            value={inputs.username || ""}
+            onChange={handleChange}
+          />
+          <label htmlFor="Email">
+            Email:
+            <input
+              name="Email"
+              type="text"
+              value={inputs.Email || ""}
+              onChange={handleChange}
+            ></input>
+          </label>
+          <label htmlFor="password">Password:</label>
+          <input
+            name="password"
+            type="password"
+            value={inputs.password || ""}
+            onChange={handleChange}
+          ></input>
+          <button type="submit" className="submit-button">
+            Sign Up
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
 
-  export default Signup;
+export default Signup;
